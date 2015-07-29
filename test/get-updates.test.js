8552f18bd168401f7ebf0f7476702c96df592fff
DAB0mB <emanor6@gmail.com>
2015-07-30 00:55:12 +0300

    Add deepness level options

--- test/get-updates.test.js
+++ test/get-updates.test.js
@@ -1,9 +1,24 @@
-var GetUpdates = require('..');
+var _ = require('underscore');
 var Chai = require('chai');
+
+var GetUpdates = require('..');
 
 var expect = Chai.expect;
 
 describe('get-updates', function() {
+  describe('validations', function() {
+    it('should throw an error if first argument is not an object', function() {
+      var boundGetUpdates = _.partial(GetUpdates);
+      expect(boundGetUpdates).to.throw(Error, /first argument.*object/); 
+    });
+
+    it('should throw an error if second argument is not an object', function() {
+      var boundGetUpdates = _.partial(GetUpdates, {});
+      expect(boundGetUpdates).to.throw(Error, /second argument.*object/);
+    });
+  });
+
+  describe('general functionality', function() {
   it('should define a "$set" property when the destination object has extra properties', function() {
     var src = {
       a: 'a',
@@ -82,5 +97,65 @@ describe('get-updates', function() {
 
     var actualUpdates = GetUpdates(src, dst);
     expect(actualUpdates).to.deep.equal(expectedUpdates);
+  });
+});
+
+  describe('"isShallow" option', function() {
+    it('should return shallow updates if option is truthy', function() {
+      var src = {
+        a: 'a',
+        b: {
+          c: 'c'
+        }
+      };
+
+      var dst = {
+        a: 'changed',
+        b: {
+          c: 'changed'
+        }
+      };
+
+      var expectedUpdates = {
+        $set: {
+          'a': 'changed',
+        }
+      };
+
+      var actualUpdates = GetUpdates(src, dst, true);
+      expect(actualUpdates).to.deep.equal(expectedUpdates);
+    });
+
+    it('should return limited updates as specified if option is a number bigger than 1', function() {
+      var src = {
+        a: 'a',
+        b: {
+          c: 'c',
+          d: {
+            e: 'd'
+          }
+        }
+      };
+
+      var dst = {
+        a: 'changed',
+        b: {
+          c: 'changed',
+          d: {
+            e: 'changed'
+          }
+        }
+      };
+
+      var expectedUpdates = {
+        $set: {
+          'a': 'changed',
+          'b.c': 'changed'
+        }
+      };
+
+      var actualUpdates = GetUpdates(src, dst, 2);
+      expect(actualUpdates).to.deep.equal(expectedUpdates);
+    });
   });
 });
